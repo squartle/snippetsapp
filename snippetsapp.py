dd7f00ecaab26ca861659001e5c15ce03c7d3463
squartle <saquartey@gmail.com>
2015-07-06 16:03:05 -0700

    07/06 4:02p

--- snippetsapp.py
+++ snippetsapp.py
@@ -6,7 +6,6 @@ logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 def put(name, snippet):
     """
     Store a snippet with an associated name.
-
     Returns the name and the snippet
     """
     logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
@@ -14,10 +13,27 @@ def put(name, snippet):
 
 def get(name):
     """Retrieve the snippet with a given name.
-
     If there is no such snippet, ask again.
-
     Returns the snippet.
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
+
+def append(name, snippet_append):
+    """
+    Find a snippet with the associated name.
+    Return the original snippet.
+    Return the updated snippet.
+    Add an "updated" tag.
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+
+def delete(name):
+    """
+    Find a snippet with the associated name.
+    Confirm deletion.
+    Delete snippet.
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
