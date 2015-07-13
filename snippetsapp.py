f290daf3417f70b1b0379e1631eac60bf0dceb30
squartle <saquartey@gmail.com>
2015-07-13 16:16:22 -0700

    filled out put stub, pre-challenge with get

--- snippetsapp.py
+++ snippetsapp.py
@@ -1,16 +1,22 @@
 import logging
 import argparse
 import sys
+import psychopg2
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+logging.debug("Connecting to PostgreSQL")
+connection = psycopg2.connect("dbname='snippets' user='ubuntu' host='localhost'")
+logging.debug("Database connection established.")
 
 def put(name, snippet):
-    """
-    Store a snippet with an associated name.
-    Returns the name and the snippet
-    """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    # Store a snippet with an associated name.
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    cursor = connection.cursor()
+    command = "insert into snippets values (%s, %s)"
+    cursor.execute(command, (name, snippet))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
     return name, snippet
 
 def get(name):
